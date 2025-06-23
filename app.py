b03b49efd59d680aed7c830b29c629a3d754462d
Leonardo de Vasconcelos <leonardogoncalves544@gmail.com>
2025-06-23 17:09:58 -0300

    Primeiro deploy

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template_string, request
+
+app = Flask(__name__)
+clientes = []
+
+HTML = """
+<!doctype html>
+<title>Cadastro de Clientes</title>
+<h2>Cadastro de Clientes</h2>
+<form method="POST">
+  Nome: <input type="text" name="nome"><br>
+  Email: <input type="email" name="email"><br>
+  Telefone: <input type="text" name="telefone"><br>
+  <input type="submit" value="Salvar">
+</form>
+
+<h3>Clientes Cadastrados:</h3>
+<ul>
+{% for c in clientes %}
+  <li>{{ c['nome'] }} - {{ c['email'] }} - {{ c['telefone'] }}</li>
+{% endfor %}
+</ul>
+"""
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        cliente = {
+            "nome": request.form["nome"],
+            "email": request.form["email"],
+            "telefone": request.form["telefone"]
+        }
+        clientes.append(cliente)
+    return render_template_string(HTML, clientes=clientes)
+
+if __name__ == "__main__":
+    app.run()
